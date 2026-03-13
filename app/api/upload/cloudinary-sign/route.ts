b912ae25cb6809c3945dcac1d2f320b42eecd291
Eylamz <eylamzr1@gmail.com>
2026-03-13 15:25:36 +0200

    FIX PREVIOUS COMMIT

--- app/api/upload/cloudinary-sign/route.ts
+++ app/api/upload/cloudinary-sign/route.ts
@@ -23,7 +23,7 @@ export async function POST(request: NextRequest) {
     throw error;
   }
 
-  const cloudName = process.env.CLOUDINARY_CLOUD_NAME;
+  const cloudName = process.env.NEXT_PUBLIC_CLOUDINARY_CLOUD_NAME;
   const apiKey = process.env.CLOUDINARY_API_KEY;
   const apiSecret = process.env.CLOUDINARY_API_SECRET;
 
